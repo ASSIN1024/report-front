60844bd4e41fd7822ddae45eb82a297dfbeec972
Developer <developer@report-platform.com>
2026-04-06 23:44:10 +0800

    feat(pipeline): 添加TriggerJob的Quartz配置
    - 新增 triggerJobDetail Bean
    - 新增 triggerJobTrigger Bean
    - 轮询间隔设置为1分钟

--- report-backend/src/main/java/com/report/common/config/QuartzConfig.java
+++ report-backend/src/main/java/com/report/common/config/QuartzConfig.java
@@ -1,6 +1,7 @@
 package com.report.common.config;
 
 import com.report.job.FtpScanJob;
+import com.report.trigger.TriggerJob;
 import org.quartz.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,6 +28,30 @@ public class QuartzConfig {
                 .forJob(ftpScanJobDetail())
                 .withIdentity("ftpScanTrigger")
                 .withDescription("FTP扫描触发器")
+                .withSchedule(scheduleBuilder)
+                .startNow()
+                .build();
+    }
+
+    @Bean
+    public JobDetail triggerJobDetail() {
+        return JobBuilder.newJob(TriggerJob.class)
+                .withIdentity("triggerJob")
+                .withDescription("数据触发器轮询任务")
+                .storeDurably()
+                .build();
+    }
+
+    @Bean
+    public Trigger triggerJobTrigger() {
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+                .withIntervalInMinutes(1)
+                .repeatForever();
+
+        return TriggerBuilder.newTrigger()
+                .forJob(triggerJobDetail())
+                .withIdentity("triggerJobTrigger")
+                .withDescription("数据触发器轮询触发器")
                 .withSchedule(scheduleBuilder)
                 .startNow()
                 .build();
