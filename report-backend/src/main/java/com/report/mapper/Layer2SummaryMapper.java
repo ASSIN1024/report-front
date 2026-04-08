e6d7f5b32665f957dc5b20a94ea088fb9bd32691
Developer <developer@report-platform.com>
2026-04-09 00:46:54 +0800

    feat(mapper): Add all Mapper interfaces for pipeline entities

---
+++ report-backend/src/main/java/com/report/mapper/Layer2SummaryMapper.java
@@ -0,0 +1,9 @@
+package com.report.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.report.entity.dws.Layer2Summary;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface Layer2SummaryMapper extends BaseMapper<Layer2Summary> {
+}
