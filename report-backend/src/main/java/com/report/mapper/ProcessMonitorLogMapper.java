9fd37f1fc4ceb0ca80df59560ffda967167e2830
Developer <developer@report-platform.com>
2026-05-05 00:37:20 +0800

    feat: 添加ProcessMonitorLogMapper

---
+++ report-backend/src/main/java/com/report/mapper/ProcessMonitorLogMapper.java
@@ -0,0 +1,9 @@
+package com.report.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.report.entity.ProcessMonitorLog;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProcessMonitorLogMapper extends BaseMapper<ProcessMonitorLog> {
+}
